1b6cb3564376f9a75a232f6ba90357018bb4a99a
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-02 22:18:17 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ParentComponent from './ParentComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React.memo() Guided Project</h1>
+      <ParentComponent />
+    </div>
+  );
+};
+
+export default App;
